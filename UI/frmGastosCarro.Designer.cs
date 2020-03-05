0e9bccadff3795fc57534c7645c08055e1edaa7c
jlalvescarvalho <lucianoalves413@gmail.com>
2020-03-05 15:58:53 -0300

    Novo repositorio

---
+++ UI/frmGastosCarro.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace UI
+{
+    partial class frmGastosCarro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource2 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmGastosCarro));
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.GastosBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.MovimentacaoBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.GastosBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MovimentacaoBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // reportViewer1
+            // 
+            this.reportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            reportDataSource1.Name = "DataSet1";
+            reportDataSource1.Value = this.GastosBindingSource;
+            reportDataSource2.Name = "DataSet2";
+            reportDataSource2.Value = this.MovimentacaoBindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource2);
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "UI.relatorio_GatosCarro.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(0, 0);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.ServerReport.BearerToken = null;
+            this.reportViewer1.Size = new System.Drawing.Size(800, 450);
+            this.reportViewer1.TabIndex = 0;
+            // 
+            // GastosBindingSource
+            // 
+            this.GastosBindingSource.DataSource = typeof(Models.Gastos);
+            // 
+            // MovimentacaoBindingSource
+            // 
+            this.MovimentacaoBindingSource.DataSource = typeof(Models.Movimentacao);
+            // 
+            // frmGastosCarro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.reportViewer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmGastosCarro";
+            this.Text = "Gastos por veículos";
+            this.Load += new System.EventHandler(this.frmGastosCarro_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.GastosBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MovimentacaoBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private System.Windows.Forms.BindingSource GastosBindingSource;
+        private System.Windows.Forms.BindingSource MovimentacaoBindingSource;
+    }
+}
